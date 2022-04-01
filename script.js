d231ec68342373a4ae3cf35c600f1c31ab81b929
Vladislav Kucherenko <vladislav.a.kucherenko@gmail.com>
2022-04-01 18:28:06 +0300

    Select adding

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function show() {
-  console.log(`  Name: ${document.Person.personname.value} \n
-  Surname: ${document.Person.personsurname.value} \n
-  Date: ${document.Person.persondate.value} \n
-  Education: ${document.Person.education.checked ? "Yes" : "No"} \n
-  Sex: ${document.Person.sex[0].checked ? "Man" : "Woman"} \n
-  End`);
+  if (document.Person.personname.value) {
+    console.log(`Name: ${document.Person.personname.value} \n
+    Surname: ${document.Person.personsurname.value} \n
+    Date: ${document.Person.persondate.value} \n
+    Education: ${document.Person.education.checked ? "Yes" : "No"} \n
+    Sex: ${document.Person.sex[0].checked ? "Man" : "Woman"} \n
+    End`);
+  }
 }
