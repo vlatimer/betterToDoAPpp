fdbfdf5eec55b1f8d477d25b6b62b7f7eaf3c7f7
Vladislav Kucherenko <vladislav.a.kucherenko@gmail.com>
2022-04-23 19:20:04 +0300

    reclassing

---
+++ Computer.js
@@ -0,0 +1,20 @@
+class Computer {
+  constructor() {
+    //pass
+  }
+  getPersonName() {
+    return document.create.createName.value;
+  }
+  getPersonSurName() {
+    return document.create.createSurname.value;
+  }
+  getPersonDate() {
+    return document.create.createDate.value;
+  }
+  getPersonEducation() {
+    return document.create.createEducation.value;
+  }
+  getPersonGender() {
+    return document.create.createGender.value;
+  }
+}
